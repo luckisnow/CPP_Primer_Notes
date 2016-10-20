2c6290770329f64889c2b9efb02544be59862226
Lucki <luckisnow@qq.com>
2016-10-20 17:59:35 +0800

    1.Change gitignore file to fit Visual Studio Project 2.Complete exercises to 11.4

--- Ch11/Chapter11/Chapter11/src.cpp
+++ Ch11/Chapter11/Chapter11/src.cpp
@@ -10,6 +10,9 @@
 
 #include<string>
 #include<iostream>
+#include<algorithm>
+#include<iterator>
+#include<cctype>
 #include <map> //map,multimap
 #include <set>  //set,multiset
 #include <unordered_map>//unordered_map,unordered_multimap
@@ -23,23 +26,54 @@ using std::string; using std::cout; using std::cin; using std::endl;
 
 void CountWordNumber_P375()
 {
+	set<string> exclude = {
+		"The","But","And","Or","An","A",
+		"the","but","and","or","an","a"
+	};
 	map<string, std::size_t>words;
 	string word;
 	while (cin>>word)
 	{
-		++words[word];//if add new key-value pair and add the value.
+		if (exclude.find(word)==exclude.end())//filter exclude word
+		{
+			++words[word];//if add new key-value pair and add the value.
+		}
 	}
+
 	for (const auto&pair:words)
 	{
 		cout << pair.first << " occurs " << words[pair.first] << " " << (words[pair.first] > 1 ? "times.":"time.") << endl;
 	}
 }
+void P114()
+{
+	set<string> exclude = {
+		"The","But","And","Or","An","A",
+		"the","but","and","or","an","a"
+	};
+	map<string, std::size_t>words;
+	string word;
+	while (cin >> word)
+	{
+		std::for_each(word.begin(), word.end(), [](char& c) {if( std::isupper(c))c=std::tolower(c); });
+		string tmpWord;
+		std::copy_if(word.begin(), word.end(), std::back_inserter(tmpWord), [](const char&c) {return std::isalpha(c); });
+		if (exclude.find(tmpWord) == exclude.end())//filter exclude word
+		{
+			++words[tmpWord];//if add new key-value pair and add the value.
+		}
+	}
 
+	for (const auto&pair : words)
+	{
+		cout << pair.first << " occurs " << words[pair.first] << " " << (words[pair.first] > 1 ? "times." : "time.") << endl;
+	}
+}
 int main()
 {
 
-	//CountWordNumber_P375();
-
+//	CountWordNumber_P375();
+	P114();
 	system("pause");
 	return 0;
 }
